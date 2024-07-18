ff9f14064031ec15ea3222034da8e9f4ee914045
Juha Halmu <jhalmu@users.noreply.github.com>
2024-07-18 23:38:08 +0000

    ddf

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,6 @@ from django.conf import settings
 from django.db import models
 from django.conf import settings
 
-<<<<<<< HEAD
-
-=======
->>>>>>> e950869 (admin, models)
 class Profile(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
@@ -14,28 +10,15 @@ class Profile(models.Model):
     website = models.URLField(blank=True)
     bio = models.CharField(max_length=240, blank=True)
 
-<<<<<<< HEAD
-
 def __str__(self):
     return self.user.get_username()
 
-
-=======
-    def __str__(self):
-        return self.user.get_username()
-    
->>>>>>> e950869 (admin, models)
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
-<<<<<<< HEAD
 
-
-=======
-    
->>>>>>> e950869 (admin, models)
 class Post(models.Model):
     title = models.CharField(max_length=255, unique=True)
     subtitle = models.CharField(max_length=255, blank=True)
@@ -46,13 +29,6 @@ class Post(models.Model):
     date_modified = models.DateTimeField(auto_now=True)
     publish_date = models.DateTimeField(blank=True, null=True)
     published = models.BooleanField(default=False)
-<<<<<<< HEAD
-    author = models.ForeignKey(Profile, on_delete=models.PROTECT)
-    tags = models.ManyToManyField(Tag, blank=True)
-
-    class Meta:
-        ordering = ['-publish_date']
-=======
 
     author = models.ForeignKey(Profile, on_delete=models.PROTECT)
     tags = models.ManyToManyField(Tag, blank=True)
@@ -63,4 +39,3 @@ class Post(models.Model):
     class Meta:
         ordering = ["-publish_date"]
         
->>>>>>> e950869 (admin, models)
